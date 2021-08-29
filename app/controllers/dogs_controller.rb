76840a845c80ff5259f5b4d4d05b65f1b3ca7ca1
mitachan-1 <youko.mita1027@gmail.com>
2021-08-30 03:26:01 +0900

    Activehasuのクラス名を修正し、統一しました。

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -7,11 +7,18 @@ class DogsController < ApplicationController
         @dog = Dog.new
     end
 
-    def create@dog = Dog.new(dog_params)
+    def create
+        @dog = Dog.new(dog_params)
         if @dog.save
             redirect_to dogs_path
         else
             render :new
         end
+    end
+
+    private
+    
+    def dog_params
+        params.require(:dog).permit(:image, :name, :year_id, :m_or_f_id, :size_id, :dog_breed_id, :date, :description).merge(user_id: current_user.id)
     end
 end
--- app/models/m_or_f.rb
+++ app/models/m_or_f.rb
@@ -1,4 +1,4 @@
-class MaleFemale < ActiveHash::Base
+class MOrF < ActiveHash::Base
     self.data = [
       { id: 1, name: '--' },
       { id: 2, name: 'オス' },
