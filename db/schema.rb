b7ea7c9502a0d946c7c78039c5d5f29b23673601
mitachan-1 <youko.mita1027@gmail.com>
2021-08-30 03:00:32 +0900

    dogsテーブルを作成しました。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_29_145408) do
+ActiveRecord::Schema.define(version: 2021_08_29_175606) do
+
+  create_table "dogs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "m_or_f_id", null: false
+    t.integer "year_id", null: false
+    t.integer "dog_breed_id", null: false
+    t.integer "size_id", null: false
+    t.date "date", null: false
+    t.text "description", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_dogs_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -34,4 +48,5 @@ ActiveRecord::Schema.define(version: 2021_08_29_145408) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "dogs", "users"
 end
