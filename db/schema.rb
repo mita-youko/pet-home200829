e098342ff8ceae2143a1f7e8eb24b854b26d4c95
mitachan-1 <youko.mita1027@gmail.com>
2021-08-30 05:09:14 +0900

    catsテーブルを作成しました。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_29_184144) do
+ActiveRecord::Schema.define(version: 2021_08_29_195854) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +31,19 @@ ActiveRecord::Schema.define(version: 2021_08_29_184144) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "cats", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "m_or_f_id", null: false
+    t.integer "year_id", null: false
+    t.integer "cat_breed_id", null: false
+    t.date "date", null: false
+    t.text "description", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_cats_on_user_id"
   end
 
   create_table "dogs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -70,5 +83,6 @@ ActiveRecord::Schema.define(version: 2021_08_29_184144) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "cats", "users"
   add_foreign_key "dogs", "users"
 end
