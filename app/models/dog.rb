7c4e15658b0e0328ed02a91196352c58ec0d4f87
mitachan-1 <youko.mita1027@gmail.com>
2021-08-30 03:48:47 +0900

    新規投稿ページのフロントエンドを実装しました。

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,4 +1,10 @@
 class Dog < ApplicationRecord
     belongs_to :user
     has_one_attached :image
+
+    extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to :m_or_f
+    belongs_to :year
+    belongs_to :size
+    belongs_to :dog_breed
 end
--- app/models/m_or_f.rb
+++ app/models/m_or_f.rb
@@ -1,10 +1,10 @@
 class MOrF < ActiveHash::Base
-    self.data = [
-      { id: 1, name: '--' },
-      { id: 2, name: 'オス' },
-      { id: 3, name: 'メス' }
-    ]
-　
-    include ActiveHash::Associations
-    has_many :dogs
+  self.data = [
+    { id: 1, name: '--' },
+    { id: 2, name: 'オス' },
+    { id: 3, name: 'メス' }
+  ]
+
+  include ActiveHash::Associations
+  has_many :dogs
 end
