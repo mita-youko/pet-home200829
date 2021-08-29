d845fe36a8d2f96d54ea738676288e699200d1ba
mitachan-1 <youko.mita1027@gmail.com>
2021-08-30 03:16:49 +0900

    新規投稿ページ実装準備をしました。

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -2,4 +2,16 @@ class DogsController < ApplicationController
     def index
         @dogs = Dog.all.order(date: :asc)
     end
+
+    def new
+        @dog = Dog.new
+    end
+
+    def create@dog = Dog.new(dog_params)
+        if @dog.save
+            redirect_to dogs_path
+        else
+            render :new
+        end
+    end
 end
