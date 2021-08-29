2df4edbee8024a8e7240ac3b328dbf9ad4f340cd
mitachan-1 <youko.mita1027@gmail.com>
2021-08-30 03:51:56 +0900

    バリデーションを追加しました。

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -7,4 +7,17 @@ class Dog < ApplicationRecord
     belongs_to :year
     belongs_to :size
     belongs_to :dog_breed
+
+    with_options presence: true do
+        validates :image
+        validates :name
+        validates :date
+        validates :description
+        with_options numericality: { other_than: 1, message: "can't be blank" } do
+            validates :m_or_f_id
+            validates :year_id
+            validates :dog_breed_id
+            validates :size_id
+        end
+    end
 end
