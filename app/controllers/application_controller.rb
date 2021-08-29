49674e9cc9fda4a9fb1b3716f6b488b287942768
mitachan-1 <youko.mita1027@gmail.com>
2021-08-30 00:56:37 +0900

    ログイン機能を実装しました。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: [:member_type_id, :nickname, :name, :name_kana, :gender_id, :postal_code, :prefecture_id, :city, :building, :phone_number])
+  end
 end
