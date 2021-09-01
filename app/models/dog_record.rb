15c471aed58b2d957d797fa008ede8dc588e1b0b
mitachan-1 <youko.mita1027@gmail.com>
2021-09-01 23:12:51 +0900

    バリデーションを追加しました。

--- app/models/dog_record.rb
+++ app/models/dog_record.rb
@@ -1,3 +1,13 @@
 class DogRecord < ApplicationRecord
     belongs_to :dog
+
+    with_options presence: true do
+        validates :checkbox
+        validates :phone_number, format: { with: /\A\d{10,11}\z/ }
+
+        with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} do
+            validates :new_parent_name
+            validates :parent_name
+        end
+    end 
 end
