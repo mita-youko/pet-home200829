015e41d5d1758bba40391b8d4902fe1b7192208d
mitachan-1 <youko.mita1027@gmail.com>
2021-09-02 00:01:51 +0900

    漢字の間違いを修正しました。

--- app/models/year.rb
+++ app/models/year.rb
@@ -3,7 +3,7 @@ class Year < ActiveHash::Base
       { id: 1, name: '--' },
       { id: 2, name: '1歳未満' },
       { id: 3, name: '1歳〜5歳' },
-      { id: 4, name: '6歳〜10際' },
+      { id: 4, name: '6歳〜10歳' },
       { id: 5, name: '11歳〜15歳'},
       { id: 6, name: '16歳〜20歳'},
       { id: 7, name: '21歳〜25歳'}
